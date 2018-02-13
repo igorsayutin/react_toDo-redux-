39b4367984e73d8c258ed6edb884687dcbbd2a8e
Igor <igor.sayutin@chisw.com>
2018-02-13 15:02:57 +0200

    sagas1.1

--- src/modules/todo/weather/sagas.js
+++ src/modules/todo/weather/sagas.js
@@ -3,24 +3,13 @@ import { fetchWeatherForecastSuccess } from "./actionsWeather";
 
 export default function* onFetchWeather() {
   const location_url = `http://ip-api.com/json`;
-  const weather_url = `http://api.openweathermap.org/data/2.5/weather?appid=3b2dce7c397645e8583f51b27d0279dc&units=metric`;
-  let target_url = yield call(function* fetchLocation() {
-    try {
-      const location = yield fetch(location_url).then(response =>
-        response.json()
-      );
-      let weatherByLocation = `${weather_url}&lat=${location.lat}&lon=${
-        location.lon
-      }`;
-      return weatherByLocation;
-    } catch (e) {
-      yield put(console.log(e));
-      return;
-    }
-  });
+  const weather_url = `http://api.openweathermap.org/data/2.5/weather?appid=3b2dce7c397645e8583f51b27d0279dc&units=metric&q=Kharkiv`;
+
   yield call(function* fetchWeatherByLocation() {
     try {
-      const weather = yield fetch(target_url).then(response => response.json());
+      const weather = yield fetch(weather_url).then(response =>
+        response.json()
+      );
       yield put(fetchWeatherForecastSuccess(weather));
     } catch (e) {
       yield put(console.log(e));
@@ -28,3 +17,18 @@ export default function* onFetchWeather() {
     }
   });
 }
+
+//   let target_url = yield call(function* fetchLocation() {
+//     try {
+//       const location = yield fetch(location_url).then(response =>
+//         response.json()
+//       );
+//       let weatherByLocation = `${weather_url}&lat=${location.lat}&lon=${
+//         location.lon
+//       }`;
+//       return weatherByLocation;
+//     } catch (e) {
+//       yield put(console.log(e));
+//       return;
+//     }
+//   });
