96a1a8a2fda2968194b9083a47ef4a7f35c50d38
Igor <igor.sayutin@chisw.com>
2018-02-13 15:09:21 +0200

    sagas2

--- src/modules/todo/weather/sagas.js
+++ src/modules/todo/weather/sagas.js
@@ -2,14 +2,25 @@ import { put, all, call } from "redux-saga/effects";
 import { fetchWeatherForecastSuccess } from "./actionsWeather";
 
 export default function* onFetchWeather() {
-  const location_url = `http://ip-api.com/json`;
-  const weather_url = `http://api.openweathermap.org/data/2.5/weather?appid=3b2dce7c397645e8583f51b27d0279dc&units=metric&q=Kharkiv`;
-
-  yield call(function* fetchWeatherByLocation() {
+  const location_url = `https://ipapi.co/json`;
+  const weather_url = `http://api.openweathermap.org/data/2.5/weather?appid=3b2dce7c397645e8583f51b27d0279dc&units=metric`;
+  let target_url = yield call(function* fetchLocation() {
     try {
-      const weather = yield fetch(weather_url).then(response =>
+      const location = yield fetch(location_url).then(response =>
         response.json()
       );
+      let weatherByLocation = `${weather_url}&lat=${location.latitude}&lon=${
+        location.longitude
+      }`;
+      return weatherByLocation;
+    } catch (e) {
+      yield put(console.log(e));
+      return;
+    }
+  });
+  yield call(function* fetchWeatherByLocation() {
+    try {
+      const weather = yield fetch(target_url).then(response => response.json());
       yield put(fetchWeatherForecastSuccess(weather));
     } catch (e) {
       yield put(console.log(e));
@@ -17,18 +28,3 @@ export default function* onFetchWeather() {
     }
   });
 }
-
-//   let target_url = yield call(function* fetchLocation() {
-//     try {
-//       const location = yield fetch(location_url).then(response =>
-//         response.json()
-//       );
-//       let weatherByLocation = `${weather_url}&lat=${location.lat}&lon=${
-//         location.lon
-//       }`;
-//       return weatherByLocation;
-//     } catch (e) {
-//       yield put(console.log(e));
-//       return;
-//     }
-//   });
