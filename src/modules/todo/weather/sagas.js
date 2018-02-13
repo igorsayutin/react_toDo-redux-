4cd60191e54d3bc81c4f99215bcc88fc242328d7
Igor <igor.sayutin@chisw.com>
2018-02-13 16:20:12 +0200

    2func

--- src/modules/todo/weather/sagas.js
+++ src/modules/todo/weather/sagas.js
@@ -1,30 +1,33 @@
-// import { put, all, call } from "redux-saga/effects";
-// import { fetchWeatherForecastSuccess } from "./actionsWeather";
+import { put, all, call, select } from "redux-saga/effects";
+import { fetchWeatherByLocation, fetchLocation } from "./actionsWeather";
 
-// export default function* onFetchWeather() {
-//   const location_url = `https://ipapi.co/json`;
-//   const weather_url = `http://api.openweathermap.org/data/2.5/weather?appid=3b2dce7c397645e8583f51b27d0279dc&units=metric`;
-//   let target_url = yield call(function* fetchLocation() {
-//     try {
-//       const location = yield fetch(location_url).then(response =>
-//         response.json()
-//       );
-//       let weatherByLocation = `${weather_url}&lat=${location.latitude}&lon=${
-//         location.longitude
-//       }`;
-//       return weatherByLocation;
-//     } catch (e) {
-//       yield put(console.log(e));
-//       return;
-//     }
-//   });
-//   yield call(function* fetchWeatherByLocation() {
-//     try {
-//       const weather = yield fetch(target_url).then(response => response.json());
-//       yield put(fetchWeatherForecastSuccess(weather));
-//     } catch (e) {
-//       yield put(console.log(e));
-//       return;
-//     }
-//   });
-// }
+export default function* onFetchWeather() {
+  const locationURL = `https://ipapi.co/json`;
+  const weatherURL = `http://api.openweathermap.org/data/2.5/weather?appid=3b2dce7c397645e8583f51b27d0279dc&units=metric`;
+
+  yield call(function* fetchLocation() {
+    try {
+      const location = yield fetch(locationURL).then(response =>
+        response.json()
+      );
+      yield put(fetchLocation(location));
+    } catch (e) {
+      yield put(console.log(e));
+      return;
+    }
+  });
+
+  yield call(function* fetchWeatherByLocation() {
+    try {
+      const state = yield select();
+      const targetURL = `${weatherURL}&lat=${state.weather.location.lat}&lon=${
+        state.weather.location.lon
+      }`;
+      const weather = yield fetch(targetURL).then(response => response.json());
+      yield put(fetchWeatherByLocation(weather));
+    } catch (e) {
+      yield put(console.log(e));
+      return;
+    }
+  });
+}
